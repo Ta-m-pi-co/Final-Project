abaf93ea4a75af2ce464d7442396334366c79c9f
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-03-15 21:33:07 +0000

    conn

---
+++ Admin/admin_login.php
@@ -0,0 +1,20 @@
+<?php
+include '../components/connect.php'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=, initial-scale=1.0">
+  <title>login</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ components/connect.php
@@ -0,0 +1,7 @@
+<?php
+$db_name = 'mysql:host=localhost;dbname=cw1019_Final_Project_Database';
+$user_name = 'cw1019_FPAdmin';
+$user_password ='9Q7m2EejbgJb';
+
+
+$conn = new PDO($db_name, $user_name, $user_password);
