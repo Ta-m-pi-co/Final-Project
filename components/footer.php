1339f33ffb8682b0353611f0f58da3a4b143323f
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-04-16 13:13:18 +0100

    footer

--- components/footer.php
+++ components/footer.php
@@ -3,17 +3,39 @@
   <section class="box-container">
 
     <div class="box">
+      <h3>Help</h3>
       <a href="../main/home.php">Home</a>
-      <a href="../main/productDisplay.php">Products</a>
-      <a href="../main/orders.php">Orders</a>
       <a href="../main/contact.php">Contact</a>
       <a href="../main/about.php">About</a>
+      <a href="../main/userRegister.php">Register</a>
+      <a href="../main/userLogin.php">Login</a>
     </div>
 
+    <div class="box">
+      <h3>Shopping</h3>
+      <a href="../main/productDisplay.php">Store</a>
+      <a href="../main/orders.php">Orders</a>
+      <a href="../main/basket.php">Basket</a>
+      <a href="../main/wishlist.php">Wishlist</a>
+
+    </div>
+
+    <div class="box">
+      <h3>Contact info</h3>
+      <a href="mailto:cwoodbury1@uni.brighton.ac.uk" class="fal fa-envelope-open">cwoodbury1@uni.brighton.ac.uk</a>
+    </div>
+
+    <div class="box">
+      <h3>Social Media</h3>
+      <a href="#" class="fab fa-facebook-square">Facebook</a>
+      <a href="#" class="fab fa-instagram-square">Instagram</a>
+      <a href="#" class="fab fa-twitter-square">Twitter</a>
+
+    </div>
 
   </section>
 
-
+  <div class="credit"> &copy; copyright @ <?= date('Y'); ?></div>
 
 
 
