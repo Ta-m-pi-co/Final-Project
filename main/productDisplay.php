ca8b9fbc0115c64d30bb6dc34de9ea73e6d7f2db
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-04-16 13:38:29 +0100

    add header and footer component to pages

---
+++ main/productDisplay.php
@@ -0,0 +1,50 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+if (isset($_SESSION['userID'])) {
+
+  $userID = $_SESSION['userID'];
+} else {
+  $userID = '';
+}
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
+
+  <link rel="stylesheet" href="../css/styleMain.css">
+</head>
+
+<body>
+
+  <?php include '../components/headerUser.php'; ?>
+
+
+
+
+
+
+
+
+
+
+  <?php include '../components/footer.php'; ?>
+  <script src="../javascript/main.js"></script>
+</body>
+
+</html>
