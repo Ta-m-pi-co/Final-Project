ae716effdf7ab8a32f9411c71d2fc5a814313960
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-04-17 18:10:02 +0100

    item view

--- main/itemView.php
+++ main/itemView.php
@@ -34,7 +34,76 @@ if (isset($_SESSION['userID'])) {
 
   <?php include '../components/headerUser.php'; ?>
 
+  <section class="itemView">
 
+
+
+
+    <?php
+    $productID = $_GET['productID'];
+    $selectProducts = $conn->prepare("SELECT * FROM `products` WHERE id = ?");
+    $selectProducts->execute([$productID]);
+
+    if ($selectProducts->rowCount() > 0) {
+      while ($fetchProducts = $selectProducts->fetch(PDO::FETCH_ASSOC)) {
+    ?>
+        <h1 class="heading"><?= $fetchProducts['name']; ?></h1>
+        <form action="" method="POST" class="box">
+          <input type="hidden" name="productID" value="<?= $fetchProducts['id']; ?>">
+          <input type="hidden" name="name" value="<?= $fetchProducts['name']; ?>">
+          <input type="hidden" name="price" value="<?= $fetchProducts['price']; ?>">
+          <input type="hidden" name="image" value="<?= $fetchProducts['image1']; ?>">
+
+
+          <div class="imageContainer">
+            <div class="mainImage">
+              <img src="../images/<?= $fetchProducts['image1']; ?>" alt="">
+            </div>
+            <div class="subImage">
+              <img src="../images/<?= $fetchProducts['image1']; ?>" alt="">
+              <img src="../images/<?= $fetchProducts['image2']; ?>" alt="">
+              <img src="../images/<?= $fetchProducts['image3']; ?>" alt="">
+            </div>
+          </div>
+
+
+          <div class="content">
+
+            <div class="name"><?= $fetchProducts['name']; ?></div>
+
+            <div class="flex">
+
+              <div class="price">£<span><?= $fetchProducts['price']; ?></div>
+
+              <input type="number" name="qty" class="qty" min="1" max="99" value="1" onkeypress="if(this.value.length == 2) return false">
+
+            </div>
+
+            <div class="details">
+              <?= $fetchProducts['details']; ?>
+            </div>
+
+
+            <div class="flexBtn">
+              <input type="submit" value="add to basket" name="addToBasket" class="btn">
+              <input type="submit" value="add to wishlist" name="addToWishlist" class="optionBtn">
+            </div>
+          </div>
+
+        </form>
+    <?php
+      }
+    } else {
+      echo '<p class="empty">no products</p>';
+    }
+
+
+    ?>
+
+
+
+
+  </section>
 
 
 
