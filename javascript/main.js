93480e8c035469e717fe45f4338fc6b0a98efbcc
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-04-17 18:39:41 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -19,3 +19,13 @@ window.onscroll = function() {
    navbar.classList.remove('active');
    profile.classList.remove('active');
 };
+
+subImages = document.querySelectorAll('.itemView .box .imageContainer .subImages img');
+mainImage = document.querySelector('.itemView .box .imageContainer .mainImage img');
+
+subImages.forEach(images =>{
+  images.onclick = () =>{
+  let src = images.getAttribute('src');
+  mainImage.src = src;
+  }
+});
