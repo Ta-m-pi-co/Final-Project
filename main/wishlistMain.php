9e0eede2f522c9cacdf58e2f548b69a828037658
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-04-18 14:04:19 +0100

    wishlist

--- main/wishlistMain.php
+++ main/wishlistMain.php
@@ -35,7 +35,47 @@ if (isset($_SESSION['userID'])) {
 
   <?php include '../components/headerUser.php'; ?>
 
+  <section class="wishlist">
+    <h1 class="heading">your wishlist</h1>
 
+    <div class="boxContainer">
+      <?
+      $selectWishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE userID = ?");
+      $selectWishlist->execute([$userID]);
+
+      if ($selectWishlist->rowCount() > 0) {
+
+        while ($fetchWishlist = $selectWishlist->fetch(PDO::FETCH_ASSOC)) {
+
+      ?>
+
+          <form action="" method="post" class="box">
+            <a href="itemView.php?productID=<?= $fetchWishlist['productID']; ?>" class="fas fa-eye"></a>
+            <button type="submit" name="delete" class="fas fa-heart-broken"></button>
+            <a href="itemView.php?productID=<?= $fetchWishlist['productID']; ?>"><img src="../images/<?= $fetchWishlist['image']; ?>" alt="" class="image"></a>
+            <div class="name"><?= $fetchWishlist['name']; ?></div>
+            <div class="flex">
+              <div class="price">£<?= $fetchWishlist['price']; ?></div>
+              <input type="number" name="qty" class="qty" min="1" max="99" value="1" onkeypress="if(this.value.length == 2) return false">
+            </div>
+            <input type="submit" value="add to basket" name="addToBasket" class="btn">
+            <input type="submit" value="delete from wishlist?" name="delete" class="deleteBtn">
+          </form>
+
+
+
+      <?php
+        }
+      } else {
+        echo '<p class="empty">Nothing Saved to Wishlist</p>';
+      }
+      ?>
+
+    </div>
+
+
+
+  </section>
 
 
 
