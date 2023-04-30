9e6579a780b3069ae99837d31f54871cad7f0122
christian <65200043+Ta-m-pi-co@users.noreply.github.com>
2023-04-30 12:40:47 +0100

    redirect

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -3,12 +3,19 @@ include '../components/connect.php';
 
 session_start();
 
-$admin_id = $_SESSION['admin_id'];
+//$admin_id = $_SESSION['admin_id'];
 
-if (!isset($admin_id)) {
+//if (!isset($admin_id)) {
+//header('location:admin_login.php');
+//}
+
+if (isset($_SESSION['admin_id'])) {
+
+  $admin_id = $_SESSION['admin_id'];
+} else {
+  $admin_id = '';
   header('location:admin_login.php');
 }
-
 ?>
 
 <!DOCTYPE html>
